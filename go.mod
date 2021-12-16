82f2efa7b9879f1f6d850427605909763ccded47
Alvaro Ritorto <alvaro.ritorto@fundingcircle.com>
2021-12-16 11:58:16 -0300

    First commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module decks
+
+go 1.17
---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+func main() {
+	//cards := newDeck()
+	//
+	////cards.print()
+	//cards.saveToFile("my_cards")
+
+	cards := newDeckFromFile("my_cards")
+
+	cards.print()
+}
